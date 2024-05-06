6a5d175ac3d5e7a4aa61fd2a7d2d8f299b0db0f7
deniza <dskarymshakova@gmail.com>
2024-05-06 20:50:43 +0600

    My first commit

---
+++ src/main/java/com/example/codifygit/CodifyGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.codifygit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodifyGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodifyGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/codifygit/CodifyGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codifygit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodifyGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
